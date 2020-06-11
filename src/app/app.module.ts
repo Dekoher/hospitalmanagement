4f48c6c7fafe28c61309af0bfb76ef9cb9da0864
Dekoher <diegoherr92@gmail.com>
2020-06-11 02:34:44 -0500

    modifying modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,23 @@ import { NgModule } from '@angular/core';
 // Routes
 import { APP_ROUTES } from './app.routes';
 
-import { AppRoutingModule } from './app-routing.module';
+// Modules
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NotfoundpageComponent } from './shared/notfoundpage/notfoundpage.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphs1Component } from './pages/graphs1/graphs1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegistryComponent } from './login/registry/registry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NotfoundpageComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graphs1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegistryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
